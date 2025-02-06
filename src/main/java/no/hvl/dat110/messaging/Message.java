99c918b777e3e4f5c5c6341124cf3a5255790f6b
Nora Soraa <notjsoraa@icloud.com>
2025-02-06 15:09:57 +0100

    Message; implemented constructor

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -1,7 +1,5 @@
 package no.hvl.dat110.messaging;
 
-import no.hvl.dat110.TODO;
-
 public class Message {
 
 	// the up to 127 bytes of data (payload) that a message can hold
@@ -9,17 +7,20 @@ public class Message {
 
 	// construction a Message with the data provided
 	public Message(byte[] data) {
-		
+
 		// TODO - START
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.constructor("Message"));
-			
+		if (data == null || data.length > 127) {
+			throw new IllegalArgumentException(
+					"The message cannot be null or contain more than 127 bytes.");
+		}
+
+		this.data = data;
+
 		// TODO - END
 	}
 
 	public byte[] getData() {
-		return this.data; 
+		return this.data;
 	}
 
 }
