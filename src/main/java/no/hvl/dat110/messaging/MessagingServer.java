3139ac3bb885c0e68c362b1679f3285a39946d24
Nora Soraa <notjsoraa@icloud.com>
2025-02-06 17:30:35 +0100

    MessagingServer:accept(); implemented method

--- src/main/java/no/hvl/dat110/messaging/MessagingServer.java
+++ src/main/java/no/hvl/dat110/messaging/MessagingServer.java
@@ -2,8 +2,7 @@ package no.hvl.dat110.messaging;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-
-import no.hvl.dat110.TODO;
+import java.net.Socket;
 
 public class MessagingServer {
 
@@ -29,13 +28,19 @@ public class MessagingServer {
 		MessageConnection connection = null;
 
 		// TODO - START
-		// accept TCP connection on welcome socket and create messaging connection to be returned
+		// accept TCP connection on welcome socket and create messaging connection to be
+		// returned
 
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
+		try {
+			Socket clientSocket = welcomeSocket.accept();
+			connection = new MessageConnection(clientSocket);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// TODO - END
-		
+
 		return connection;
 
 	}
