9bb04283f26baea8c8f2cfc69c149c45f61b1beb
Nora Soraa <notjsoraa@icloud.com>
2025-02-06 16:32:55 +0100

    MessageConnection; implemented methods send() and receive()

--- src/main/java/no/hvl/dat110/messaging/MessageConnection.java
+++ src/main/java/no/hvl/dat110/messaging/MessageConnection.java
@@ -1,20 +1,16 @@
 package no.hvl.dat110.messaging;
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
-import no.hvl.dat110.TODO;
-
 
 public class MessageConnection {
 
 	private DataOutputStream outStream; // for writing bytes to the underlying TCP connection
 	private DataInputStream inStream; // for reading bytes from the underlying TCP connection
 	private Socket socket; // socket for the underlying TCP connection
-	
+
 	public MessageConnection(Socket socket) {
 
 		try {
@@ -23,7 +19,7 @@ public class MessageConnection {
 
 			outStream = new DataOutputStream(socket.getOutputStream());
 
-			inStream = new DataInputStream (socket.getInputStream());
+			inStream = new DataInputStream(socket.getInputStream());
 
 		} catch (IOException ex) {
 
@@ -35,13 +31,17 @@ public class MessageConnection {
 	public void send(Message message) {
 
 		byte[] data;
-		
+
 		// TODO - START
 		// encapsulate the data contained in the Message and write to the output stream
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
+		data = MessageUtils.encapsulate(message);
+
+		try {
+			outStream.writeBytes(data.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// TODO - END
 
 	}
@@ -49,30 +49,35 @@ public class MessageConnection {
 	public Message receive() {
 
 		Message message = null;
-		byte[] data;
-		
+		byte[] data = null;
+
 		// TODO - START
 		// read a segment from the input stream and decapsulate data into a Message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
+
+		try {
+			data = inStream.readAllBytes();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		message = MessageUtils.decapsulate(data);
+
 		// TODO - END
-		
+
 		return message;
-		
+
 	}
 
-	// close the connection by closing streams and the underlying socket	
+	// close the connection by closing streams and the underlying socket
 	public void close() {
 
 		try {
-			
+
 			outStream.close();
 			inStream.close();
 
 			socket.close();
-			
+
 		} catch (IOException ex) {
 
 			System.out.println("Connection: " + ex.getMessage());
