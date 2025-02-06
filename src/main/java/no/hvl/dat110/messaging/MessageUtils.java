01a69f64b66acfb25ec5a3ec783276886ad37f2a
Nora Soraa <notjsoraa@icloud.com>
2025-02-06 15:22:38 +0100

    MessageUtils:encapsulate(); implemented method

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -12,37 +12,47 @@ public class MessageUtils {
 	public static String MESSAGINGHOST = "localhost";
 
 	public static byte[] encapsulate(Message message) {
-		
+
 		byte[] segment = null;
 		byte[] data;
-		
+
 		// TODO - START
-		
+
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
+
+		data = message.getData();
+
+		if (data == null || data.length >= SEGMENTSIZE) {
+			throw new IllegalArgumentException(
+					"The message cannot be null or contain more than 127 bytes.");
+		}
+
+		segment[0] = (byte) data.length;
+
+		for (int i = 0; i < data.length; i++) {
+			segment[i + 1] = data[i];
+		}
+
 		// TODO - END
 		return segment;
-		
+
 	}
 
 	public static Message decapsulate(byte[] segment) {
 
 		Message message = null;
-		
+
 		// TODO - START
 		// decapsulate segment and put received payload data into a message
-		
+
 		if (true)
 			throw new UnsupportedOperationException(TODO.method());
-		
+
 		// TODO - END
-		
+
 		return message;
-		
+
 	}
-	
+
 }
